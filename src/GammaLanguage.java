1c4cd9d1513b08306d2ed762c78ab75af4ff421b
Ullecses <ulyana.vorobjova@gmail.com>
2024-02-08 18:08:32 +0300

    Added BinarySearch

--- src/GammaLanguage.java
+++ src/GammaLanguage.java
@@ -1,20 +1,19 @@
 import java.io.*;
 import java.util.*;
 
-public class SumOfTree {
+public class GammaLanguage {
     public static void main(String[] args) throws IOException {
         Scanner in = new Scanner(new File("input.txt"));
         PrintWriter out = new PrintWriter(new FileWriter("output.txt"));
-        long sum = 0;
-        Set<Integer> hashSet = new HashSet<>();
 
-        while (in.hasNextInt()){
-            hashSet.add(in.nextInt());
+        int m = in.nextInt();
+        int n = in.nextInt();
+
+        for (int i = 0; i < m; i++) {
+
         }
-        for (int i : hashSet) {
-            sum += i;
-        }
-        out.println(sum);
+
+        out.println();
         out.flush();
     }
 }
